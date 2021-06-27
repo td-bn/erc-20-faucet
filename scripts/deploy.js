02a55686e7033d6dbbce5b2a96aadd328966ca21
bnabi <bnabi@pop-os.localdomain>
2021-06-27 09:59:51 +0530

    Add LearnToken contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional 
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile 
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+ 
+  const [deplyoer] = await hre.ethers.getSigners();
+  console.log("Deoploying with account address: ", deplyoer.address)
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const LearnToken = await hre.ethers.getContractFactory("LearnToken");
+  const learnToken = await LearnToken.deploy("LearnToken", "LRN");
 
-  await greeter.deployed();
+  await learnToken.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("LearnToken deployed to:", learnToken.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
