177153cf7c850c1ad0b263dbfd2f588fdcedf8d7
bnabi <bnabi@pop-os.localdomain>
2021-07-27 16:50:41 +0530

    deploy to rinkeby

--- src/components/Faucet.js
+++ src/components/Faucet.js
@@ -4,7 +4,7 @@ import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import Message from './Message';
 
 // Pattern to get deployed address?
-let tokenAddress = '0xF3388099C0d9C3C1aA0392CBECa8EB18eAbC25Ca';
+let tokenAddress = '0xA94c1FCB684d4b6D95b34D149d05ceF0b00b3414';
 
 const Faucet = (props) => {
     const [balance, setBalance] = useState();
